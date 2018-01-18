1f4a3b827b77d7dc1b226c61629869b490b9eec6
paradigm777 <dflynn2@student.cccd.edu>
2018-01-17 21:09:18 -0800

    Add files via upload

--- p14.py
+++ p14.py
@@ -1,17 +1,22 @@
 # Program name: p14.py
 # Your Name: 
 # Python Version: 3.6.4
-# Date: 01/16/2018
+# Date: 01/17/2018
 # Description: 	Write a program that asks the user for day and month of a birthday.
 #               The program then tells the Zodiac signs that will be compatible with that birthday.
+#				
+# 				Example for  Aries	The Ram		Mar. 21–Apr. 19	
+#				day = 24  # user enters this
+#				month = 3 # user enters this
+#				if ( month == 3 and day >=21) or (month == 4 and day <= 19):
+#				print("You are Aries, Fire group, compatible with Aries, Leo, Sagittarius")
 #
-#               Hint, use integer casting, 66/25 equals 2.64, but int(66/25)=2.
-#               # Test 66, 16, 6, 4
 # Ask the user for input
-totalCents = int(input('Please enter the total cents: '))
+d = int(input('Please enter day of birth: '))
+m = int(input('Please enter month of birth: '))
 
 # use integer division to get a whole number for quarters
-q = int(totalCents/25)
+if ( month == 3 and day >=21)
 
 # if there are any quarters...
 if q > 0:
